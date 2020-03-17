6387c964af343ebd143102ac3987514a2e4c97cb
Deepak Choudhary <choudharydeepak990@gmail.com>
2020-03-17 23:47:13 +0530

    Update WordAdapter.java

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,4 +1,29 @@
 package com.example.android.miwok;
 
-class WordAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+class WordAdapter extends ArrayAdapter {
+    public WordAdapter(@NonNull Context context, int resource, int textViewResourceId) {
+        super(context, resource, textViewResourceId);
+    }
+
+    public WordAdapter(NumbersActivity numbersActivity, ArrayList<Word> words) {
+        super(numbersActivity,0, (List) numbersActivity);
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
 }
