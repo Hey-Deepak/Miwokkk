bf49ddf79946810e135b4b8518a1b2630ae9e0e3
Deepak Choudhary <choudharydeepak990@gmail.com>
2020-03-17 20:37:37 +0530

    Update Word.java

--- app/src/main/java/Word.java
+++ app/src/main/java/Word.java
@@ -1,2 +1,22 @@
+
+
 public class Word {
+
+    private String mDefaultTraslation;
+
+    private String mMiwokTraslation;
+
+    public Word(String defaultTraslation, String miwokTraslation){
+
+        mDefaultTraslation = defaultTraslation;
+        mMiwokTraslation = miwokTraslation;
+    }
+
+    public String getDefaultTraslation(){
+        return mDefaultTraslation;
+    }
+
+    public String getMiwokTraslation(){
+        return mMiwokTraslation;
+    }
 }
