ab7cd115530ee5caadd057793410d7990095c83a
Hey-Deepak <choudharydeepak990@gmail.com>
2020-03-28 04:01:03 +0530

    ....

---
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -0,0 +1,46 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+class CategoryAdapter extends FragmentPagerAdapter {
+
+    private Context mContext;
+    public CategoryAdapter(Context mContext, FragmentManager fm) {
+        super(fm);
+        this.mContext = mContext;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new NumbersFragment();
+        } else if (position == 1){
+            return new FamilyFragment();
+        } else if (position ==2){
+            return new ColorsFragment();
+        }else
+            return new PhrasesFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
+    }
+}
